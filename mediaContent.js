723c8367330e6c9082b26d0e8c3d589409697b8a
PetrXleb <129799761+PetrXleb@users.noreply.github.com>
2023-08-03 02:40:56 +0300

    Add files via upload

---
+++ mediaContent.js
@@ -0,0 +1,61 @@
+//
+//ИМЕНА ПЕРСОНАЖЕЙ
+//
+let autor = {
+  name: "Автор",
+  color: "black",
+};
+
+let nameLena = {
+  name: "Лена",
+  color: "#6f02c9",
+};
+
+let you = {
+  name: "Семён",
+  color: "rgb(67, 48, 238)",
+};
+
+let thoughts = {
+  name: "&nbsp;",
+  color: "rgba(67, 48, 238, 0)",
+};
+
+let Slavya = {
+  name: "Славя",
+  color: "rgb(238, 255, 0)",
+};
+
+let Nikolai = {
+  name: "Николай",
+  color: "#d76e00",
+};
+//
+//ФОНЫ
+//
+let fon = "Media/Background/fonLager1.jpg";
+let fon1 = "Media/Background/fonLep.jpg";
+let fon2 = "Media/Background/medpunkt.jpg";
+let sqfon = "Media/Background/sq.jpg";
+let sceneFon = "Media/Background/scene.jpg";
+let NikolaiHome = "Media/Background/NicolaiHome.jpg";
+
+//
+//СПРАЙТЫ
+//
+let sprait = `https://avatanplus.com/files/resources/original/571e70a5100871544ee804ca.png`;
+let Slavya_spait1 = "Media/Sprite/slavya1.png";
+let Slavya_spait2 = "Media/Sprite/slavya2.png";
+let lenaSport = "Media/Sprite/lenaSmile.png";
+let Himicy = "Media/Sprite/Himicy.png";
+
+//
+//МУЗЫКА
+//
+let music1 = "Media/Music/everlastingSummer.mp3";
+let music2 = "Media/Music/DancOfFireflies.mp3";
+//
+//ЗВУЕОВЫЕ ЭФФЕКТЫ
+//
+let sound1 = "Media/Sounds/pokos-suhoy-travyi.mp3";
+let applebite = "Media/Sounds/applebite.mp3";
