b6b4d7849571aec03ca9d313d9f5112cd94c27c2
PetrXleb <129799761+PetrXleb@users.noreply.github.com>
2023-08-04 00:19:31 +0300

    Version 1.1
    Add Save, speedText and other stuff

--- script.js
+++ script.js
@@ -7,12 +7,22 @@ const DivBackground = document.querySelector(".divBackground");
 const Footer = document.querySelector(".footer");
 const IconVie = document.querySelector(".fa-eye");
 const IconSave = document.querySelector(".fa-folder");
+const NewGameBtn = document.querySelector(".NewGameBtn");
+const LoadGameBtn = document.querySelector(".LoadGameBtn");
 
 let STARTFLAG = true;
 let INDEX = 0;
 let TEMP;
 let MUSIC;
 let SOUND;
+let SPEEDTEXT = 15; //от 1 до 30 (по умолчанию 15)
+
+let TempToLocalStorage = {
+  color: "none",
+  background: "none",
+  music: "none",
+};
+let spriteArrTolocalStorage = [];
 
 //
 //Готовые функции
@@ -26,12 +36,14 @@ function evaluateFunction(fnStr, param) {
 //смена фона
 function FNchangeBackgroundImage(url) {
   DivBackground.style.backgroundImage = `url(${url})`;
+  TempToLocalStorage.background = url;
 }
 
 //смена имени
 function FNchangeName(name) {
   Name.innerHTML = name.name;
   Name.style.color = name.color;
+  TempToLocalStorage.color = name;
 }
 
 //ставим спрайт
@@ -40,6 +52,7 @@ function FNputSprait(SpraitArr) {
   let size = SpraitArr[1];
   let top = SpraitArr[2];
   let left = SpraitArr[3];
+  let delay = SpraitArr[4];
   let divContainer = document.createElement("div");
   // Задание стиля
   divContainer.style.backgroundImage = `url(${sprait})`;
@@ -52,11 +65,18 @@ function FNputSprait(SpraitArr) {
   divContainer.style.backgroundSize = `${size}%`;
   divContainer.style.top = `${top}%`;
   divContainer.style.left = `${left}%`;
+  //задержка при появлении
+  setTimeout(() => {
+    divContainer.style.opacity = "1";
+  }, delay);
   //добавляем спец класс, чтобы потом удалять спрайты
   let classDelete = sprait.replace(/[.:/]/g, "");
+  divContainer.classList.add("spraitTransform");
   divContainer.classList.add(classDelete);
   // Добавление созданного элемента в <body>
   document.body.appendChild(divContainer);
+  //Спрайт в spriteArrTolocalStorage
+  spriteArrTolocalStorage.push(SpraitArr);
 }
 
 //удалить спрайт
@@ -64,10 +84,20 @@ function FNdeleteSprait(Sprait) {
   let classDelete = Sprait.replace(/[.:/]/g, "");
   let SpraitToDelete = document.querySelector(`.${classDelete}`);
   SpraitToDelete.parentNode.removeChild(SpraitToDelete);
+  //удалить из spriteArrTolocalStorage
+  spriteArrTolocalStorage;
+  for (let i = 0; i < spriteArrTolocalStorage.length; i++) {
+    let tempSprite = spriteArrTolocalStorage[0];
+    let tempUrl = tempSprite[0];
+    if (tempUrl == Sprait) {
+      spriteArrTolocalStorage.splice(i, 1);
+    }
+  }
 }
 
 //ставим удаляем музыку
 function FNmusic(url) {
+  TempToLocalStorage.music = `none`;
   try {
     MUSIC.pause();
     if (typeof url == "function") return;
@@ -76,6 +106,7 @@ function FNmusic(url) {
     MUSIC = new Audio(url);
     MUSIC.loop = true;
     MUSIC.play();
+    TempToLocalStorage.music = url;
   }
 }
 
@@ -141,7 +172,7 @@ function Choice(root) {
     document.body.appendChild(divWrapper);
     setTimeout(() => {
       divWrapper.parentNode.removeChild(divWrapper);
-    }, TEMP.length * 8);
+    }, TEMP.length * SPEEDTEXT);
     // Бегущая строка
     let str = root + ` `,
       i = 0,
@@ -158,7 +189,7 @@ function Choice(root) {
       if (char === ">") isTag = false;
 
       if (isTag) return type();
-      setTimeout(type, 8);
+      setTimeout(type, SPEEDTEXT);
     })();
     // Phrase.innerHTML = root;
   } else {
@@ -186,16 +217,22 @@ function Choice(root) {
 //При клике итерируется массив
 //
 DivButton.addEventListener("click", () => {
-  //если END
-  if (mainArr[INDEX] == "END") {
-    console.log("Конец");
-    return;
-  }
-  //описание в начале
+  //Действия при первом клике
   if (STARTFLAG) {
     STARTFLAG = false;
     let DescriptionDiv = document.querySelector(".description");
     DescriptionDiv.classList.add("hidden");
+    //кнопки меню
+    NewGameBtn.classList.add("hidden");
+    LoadGameBtn.classList.add("hidden");
+    IconVie.classList.remove("hidden");
+    IconSave.classList.remove("hidden");
+  }
+
+  //если END
+  if (mainArr[INDEX] == "END") {
+    console.log("Конец");
+    return;
   }
 
   let tempArr = mainArr[INDEX];
@@ -219,7 +256,7 @@ function Game() {
       document.body.appendChild(divWrapper);
       setTimeout(() => {
         divWrapper.parentNode.removeChild(divWrapper);
-      }, TEMP.length * 8);
+      }, TEMP.length * SPEEDTEXT);
       // Бегущая строка
       let str = TEMP + ` `,
         i = 0,
@@ -236,7 +273,7 @@ function Game() {
         if (char === ">") isTag = false;
 
         if (isTag) return type();
-        setTimeout(type, 8);
+        setTimeout(type, SPEEDTEXT);
       })();
       // Phrase.innerHTML = TEMP;
     } else {
@@ -250,21 +287,150 @@ function Game() {
   } catch (error) {}
 }
 
+//Функция сохранения
+IconSave.addEventListener("click", () => {
+  //Стили
+  IconSave.classList.toggle("btnClick");
+  setTimeout(() => {
+    IconSave.classList.toggle("btnClick");
+  }, 150);
+  // Преобразуем массив в строку JSON и в localStorage
+  let arrayString = JSON.stringify(mainArr);
+  localStorage.setItem("VNmySave", arrayString);
+  localStorage.setItem("VNIndexSave", INDEX - 1);
+  //
+  TempToLocalStorage = JSON.stringify(TempToLocalStorage);
+  localStorage.setItem("VNother", TempToLocalStorage);
+  //
+  spriteArrTolocalStorage = JSON.stringify(spriteArrTolocalStorage);
+  localStorage.setItem("VNSprite", spriteArrTolocalStorage);
+});
+
+//Кнопка новой игры
+NewGameBtn.addEventListener("click", () => {
+  // Получаем строку JSON из localStorage
+  if (localStorage.getItem("VNIndexSave") == null) {
+  } else {
+    localStorage.removeItem("VNmySave");
+    localStorage.removeItem("VNIndexSave");
+    localStorage.removeItem("VNother");
+    localStorage.removeItem("VNSprite");
+  }
+  //старт
+  Game();
+  NewGameBtn.classList.add("hidden");
+  LoadGameBtn.classList.add("hidden");
+  let DescriptionDiv = document.querySelector(".description");
+  DescriptionDiv.classList.add("hidden");
+  IconVie.classList.remove("hidden");
+  IconSave.classList.remove("hidden");
+});
+
+//Кнопка последнего сохранения
+LoadGameBtn.addEventListener("click", () => {
+  // Получаем строку JSON из localStorage
+  if (localStorage.getItem("VNIndexSave") == null) {
+    LoadGameBtn.innerHTML = "У вас нет сохранений!";
+    setTimeout(() => {
+      LoadGameBtn.innerHTML = "Последнее сохранение";
+    }, 1300);
+  } else {
+    let retrievedArrayString = localStorage.getItem("VNmySave");
+    let retrievedArray = JSON.parse(retrievedArrayString);
+    mainArr = [];
+    mainArr = retrievedArray;
+    INDEX = +localStorage.getItem("VNIndexSave");
+    //Другие данные
+    let otherData = localStorage.getItem("VNother");
+    otherData = JSON.parse(otherData);
+    LoadMedia(otherData);
+    //спрайты
+    let SpriteFromLocalStorage = localStorage.getItem("VNSprite");
+    SpriteFromLocalStorage = JSON.parse(SpriteFromLocalStorage);
+    LoadSprite(SpriteFromLocalStorage);
+    //
+    //старт
+    Game();
+    NewGameBtn.classList.add("hidden");
+    LoadGameBtn.classList.add("hidden");
+    let DescriptionDiv = document.querySelector(".description");
+    DescriptionDiv.classList.add("hidden");
+    IconVie.classList.remove("hidden");
+    IconSave.classList.remove("hidden");
+  }
+});
+
+//Функция востанавливающая Медиа после загрузки сохранения
+function LoadMedia(otherData) {
+  //
+  try {
+    FNchangeName(otherData.color);
+    FNchangeBackgroundImage(otherData.background);
+    FNmusic(otherData.music);
+  } catch (error) {}
+  //
+  TempToLocalStorage.color = otherData.color;
+  TempToLocalStorage.background = otherData.background;
+  TempToLocalStorage.music = otherData.music;
+}
+//Функция востанавливающая Спрайты после загрузки сохранения
+function LoadSprite(SpriteArr) {
+  for (let i = 0; i < SpriteArr.length; i++) {
+    FNputSprait(SpriteArr[i]);
+  }
+}
+
+//слайдер скорости текста
+let slider = document.getElementById("slider");
+let displaySlider = document.getElementById("display");
+slider.addEventListener("input", function () {
+  let value = parseInt(slider.value);
+  displaySlider.textContent = `Быстро--${value}--Медленно`;
+  SPEEDTEXT = value;
+  localStorage.setItem("VNSpeedText", SPEEDTEXT);
+});
+
+if (localStorage.getItem("VNSpeedText") == null) {
+  localStorage.setItem("VNSpeedText", SPEEDTEXT);
+} else {
+  SPEEDTEXT = +localStorage.getItem("VNSpeedText");
+  slider.value = SPEEDTEXT;
+}
 //
 //
-//Тестовый рутlet
 //
-let mainArr = [...rootDemoStart];
 //
 //
 //mainArr = [...ROOTDemoFinal];
 
-//
-//
-//
+/* let mainArr = [
+  "1111111111111",
+  "FNchangeBackgroundImage",
+  sceneFon,
+  "...",
+  "К моему удивлению, я обнаружил Лену, тренирующую тенисные подачи",
+  "FNchangeName",
+  nameLena,
+  "FNputSprait",
+  [lenaSport, 15, 30, 50], ///size //top //left
+  "Ой, привет. А ты наверное Алису ищешь?",
+  "FNchangeName",
+  you,
+  "Да нет, просто решил прогуляться",
+  "FNchangeName",
+  thoughts,
+  "~Я не стал уточнять у неё, почему она выбрала сцену, в качестве места для занятия тенисом~",
+  "FNchangeName",
+  autor,
+  "И так далее...",
+  ROOTDemoFinal,
+]; */
 
-//Функция сохранения
-IconSave.addEventListener("click", () => {
-  console.log("Функция сохранения");
-  alert("Функция сохранения ещё не готова!");
-});
+//
+//
+//
+//
+//
+//
+//Тестовый рут
+let mainArr = [...rootDemoStart];
